4ef6a48ac5e50cf143ff0f940a0fc5a3280ec1d4
ResonanceL <terephthalate@gmail.com>
2021-07-06 17:32:43 +0800

    Initial Commit

---
+++ Sources/Ron/Ron.swift
@@ -0,0 +1,3 @@
+struct Ron {
+    var text = "Hello, World!"
+}
---
+++ Tests/RonTests/RonTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import Ron
+
+    final class RonTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(Ron().text, "Hello, World!")
+        }
+    }
